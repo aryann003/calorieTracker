d4dc6373995cd99b4171a7e3d2f4bea4c6720505
araynn003 <aryankatiyar642@gmail.com>
2026-02-01 21:30:47 +0530

    changes in index.html,views.py

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,19 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Sum
 from .models import Consume, Food
 
 def index(request):
+    foods = Food.objects.all()
+
     if request.method == 'POST':
-        food_consumed = request.POST['food_consumed']
-        consume = Food.objects.get(name=food_consumed)
-        user = request.user
-        consume = Consume(user=user, food_consumed=consume)
-        consume.save()
-        foods = Food.objects.all()
+        food_id = request.POST.get('food_consumed')
+        food = get_object_or_404(Food, id=food_id)
 
-    else: 
-        foods = Food.objects.all()
-    return render(request,'myapp/index.html',{'foods': foods})
+        Consume.objects.create(
+            user=request.user,
+            food_consumed=food
+        )
+        return redirect('index')
 
+    consumed_food = Consume.objects.filter(user=request.user)
 
+    totals = consumed_food.aggregate(
+        total_calories=Sum('food_consumed__calories'),
+        total_carbs=Sum('food_consumed__carbs'),
+        total_protein=Sum('food_consumed__protein'),
+        total_fat=Sum('food_consumed__fat'),
+    )
 
- 
+    CALORIE_GOAL = 2600
+
+    calPercent =0
+    if totals['total_calories']:
+        calPercent = (totals['total_calories'] / CALORIE_GOAL) * 100
+
+    context = {
+        'foods': foods,
+        'consumed_food': consumed_food,
+        'total_calories': totals['total_calories'] or 0,
+        'total_carbs': totals['total_carbs'] or 0,
+        'total_protein': totals['total_protein'] or 0,
+        'total_fat': totals['total_fat'] or 0,
+        'calPercent': round(calPercent,2),
+        'CALORIE_GOAL': CALORIE_GOAL,
+    }
+
+    return render(request, 'myapp/index.html', context)
